51969ea0f06cf8e20a9d7698f8639c8f252378d0
Ubuntu <ubuntu@ip-172-31-31-162.ap-southeast-2.compute.internal>
2019-12-12 12:59:16 +0000

    Cache plugin files

---
+++ wp-content/cache/autoptimize/js/autoptimize_snippet_00f42dcc8b6465c06ac939aa90afbcaf.js
@@ -0,0 +1,9 @@
+(function($){"use strict";function urus_init_popup(){if(urus_popup_frontend.enable_popup_mobile!=0){if($(window).innerWidth()<768){return false;}}
+var disabled_popup_by_user=getCookie('urus_disabled_popup_by_user');if(disabled_popup_by_user=='true'){return false;}else{if($('body').hasClass('urus-popup-on')&&urus_popup_frontend.enable_popup==1){setTimeout(function(){var data={action:'urus_popup_load_content',current_page_id:urus_popup_frontend.current_page_id};$.post(urus_popup_frontend.ajaxurl,data,function(response){$.magnificPopup.open({items:{src:'<div class="white-popup mfp-with-anim">'+response.content+'</div>',type:'inline',},removalDelay:500,callbacks:{open:function(){$('div.wpcf7 > form').each(function(){var $form=$(this);wpcf7.initForm($form);var form_action=$form.attr('action');var action_url='#'+form_action.split('#')[1];if(wpcf7.cached){wpcf7.refill($form);}
+$form.attr('action',action_url);});},beforeOpen:function(){this.st.mainClass=response.display_effect;}},midClick:true});});},urus_popup_frontend.delay_time);}}}
+$(document).on('change','.urus_disabled_popup_by_user',function(){if($(this).is(":checked")){setCookie("urus_disabled_popup_by_user",'true',7);if($('.mfp-close')){$('.mfp-close').trigger('click');}}else{setCookie("urus_disabled_popup_by_user",'',0);}});function setCookie(cname,cvalue,exdays){var d=new Date();d.setTime(d.getTime()+(exdays*24*60*60*1000));var expires="expires="+d.toUTCString();document.cookie=cname+"="+cvalue+"; "+expires;}
+function getCookie(cname){var name=cname+"=";var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i];while(c.charAt(0)==' '){c=c.substring(1);}
+if(c.indexOf(name)==0){return c.substring(name.length,c.length);}}
+return"";}
+$(document).ready(function(){urus_init_popup();});})
+(jQuery);
